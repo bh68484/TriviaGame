efe59ad2811f6bef34463050658e1c12f6611bef
Brandon Harrington <bh68484@gmail.com>
2018-02-21 20:44:24 -0500

    working game.  Needs timer and images to be added after answered question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,119 @@
 $(document).ready(function() {
-    //Global Variables
-    var quiz = 10;
-    var right = 0;
-    var wrong = 0;
-    var questionIndex = 0;
-    var count = 30;
-  
-    //Questions and answers set to an array
-    var questions = [{
+  //Global Variables
+  var quiz = 10;
+  var right = 0;
+  var wrong = 0;
+  var questionIndex = 0;
+  var count = 5;
+  var clock;
+
+  //Questions and answers set to an array
+  var questions = [{
       question: "Where did the Wright Brothers make the first controlled power airplane flight?",
       choices: ["Nags Head", "Hatteras Island", "Wilmington", "Kitty Hawk"],
       answer: 3
-    }, {
+  }, {
       question: "What carnivorous plant is native to the subtropical wetlands in North Carolina?",
       choices: ["Tropical Pitcher Plant", "Venus Fly Trap", " Corkscrew Plant", "Sundew Plant"],
       answer: 1
-    }, {
+  }, {
       question: "This Interstate Highway runs across the United States from Wilmington, NC to Barstow, CA",
       choices: ["I-73", "I-26", "I-85", "I-40"],
       answer: 3
-    }, {
+  }, {
       question: "Which city is the largest in North Carolina?",
       choices: ["Raleigh", "Charlotte", "Greensboro", "Durham"],
       answer: 1
-    }, {
+  }, {
       question: "How many counties are in North Carolina?",
       choices: ["78", "90", "100", "50"],
       answer: 2
-    }, {
+  }, {
       question: "What is North Carolina's Highest point in elevation?",
       choices: ["Grandfather Mountain", "Blowing Rock", "Pilot Mountain", "Mount Mitchell"],
       answer: 3
-    }, {
+  }, {
       question: "The USS North Carolina was dedicated as a memorial and museum in which city?",
       choices: ["Southport", "Wilmington", "New Bern", "Beaufort"],
       answer: 1
-    }, {
+  }, {
       question: "This lighthouse was moved 2,900 feet inland due to coastal erosion.",
       choices: ["Bodie Island Lighthouse", "Price Creek Lighthouse", "Ocracoke Lighthouse", "Cape Hatteras Lighthouse"],
       answer: 3
-    }, {
+  }, {
       question: "The waters off the North Carolina coast are often called what due to the numerous shipwrecks?",
       choices: ["Shipwreck Bay", "The Deadly Shoals", "Graveyard of the Atlantic", "Atlantic Triangle"],
       answer: 2
-    }, {
+  }, {
       question: "What popular soda was invented in 1898 in New Bern, NC?",
       choices: ["Cheerwine", "Pepsi", "Dr. Pepper", "Coca-Cola"],
       answer: 1
-    }]
-console.log(questions);
+  }]
+  console.log(questions);
+  //Loads the question and sets the answer choices to the multiple choice buttons
+  $("#0, #1, #2, #3").hide();
+
+  function loadQuestion() {
+      if (questionIndex < questions.length) {
+          //Displays the question on the page
+          $('#questions').html(questions[questionIndex].question);
+          $("#0, #1, #2, #3").show();
+          //Displays multiple choices
+          $("#0").text(questions[questionIndex].choices[0]);
+          $("#1").text(questions[questionIndex].choices[1]);
+          $("#2").text(questions[questionIndex].choices[2]);
+          $("#3").text(questions[questionIndex].choices[3]);
+      } else {
+          $("#quiz, #timer").hide("slow");
+          $("#results").show("slow");
+          scoreCount();
+      }
+  };
+  //Checks multiple choice buttons if right or wrong - adds either to right or wrong counter.
+
+  $(".answerChoices").click(function() {
+      userGuess = $(this).attr("id");
+      //check for correct answer
+      if (userGuess == questions[questionIndex].answer) {
+          correct = right++;
+          console.log(correct + " correct");
+         
+
+      } else {
+          incorrect = wrong++;
+          console.log(incorrect + " incorrect");
+      }
+      questionIndex++;
+      loadQuestion();
+  });
+
+  //Function to show the number of right and wrong answers after the end of the game.
+  function scoreCount() {
+      $('#correct').html(" " + right);
+      $('#incorrect').html(" " + wrong);
+  }
+
+  //Results are hidden until the game is over
+  $("#quiz, #results").hide();
+
+
+  //When the start button is clicked - it will hide and show the questions and choices.
+  $("#play").click(function() {
+      $("#start").hide("slow");
+      $("#quiz").show("slow");
+      loadQuestion();
+
+
+      //Sets up timer to show 30 seconds on the clock.
+  
+ 
+console.log(count);
+
+  //Restart button shown at the end of the game - allows user to click and restart the game over.
+  $("#restart").click(function() {
+      location.reload();
+  });
+
+ 
+  })
 })
